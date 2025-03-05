40f2ed19ecf19feea9112fc09a677123694e7670
nataliewxngg <wong.lt.natalie@gmail.com>
2025-03-05 16:53:55 -0500

    Add JS for library management

---
+++ script.js
@@ -0,0 +1,16 @@
+const myLibrary = [];
+
+function Book(title, author, pages, read) {
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.read = read;
+    this.id = crypto.randomUUID();
+}
+
+function addBookToLibrary(title, author, pages, read) {
+    myLibrary.push(new Book(title, author, pages, read));
+}
+
+addBookToLibrary('The Hobbit', 'J.R.R. Tolkien', 295, true);
+console.log(myLibrary);
