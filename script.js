85dffad0ef1133b39c287f6888298b4206c052b9
natalie <wong.lt.natalie@gmail.com>
2025-03-07 15:30:49 -0500

    Implement functionality for adding books

--- script.js
+++ script.js
@@ -1,12 +1,19 @@
 // VARIABLES
 const myLibrary = [];
 const library = document.getElementById('library');
-const addBook = document.getElementById('add-book');
+const addBookButton = document.getElementById('add-book');
 const dialog = document.querySelector('dialog');
+const form = document.querySelector('form');
 
 // EVENT LISTENERS
-addBook.addEventListener('click', () => {
-    dialog.showModal();
+addBookButton.addEventListener('click', () => dialog.showModal());
+
+form.addEventListener('submit', e => {
+    e.preventDefault();
+    const data = new FormData(e.target);
+
+    addBookToLibrary(data.get('title'), data.get('author'), data.get('pages'), data.get('read'));
+    dialog.close();
 });
 
 // FUNCTIONS
@@ -20,35 +27,26 @@ function Book(title, author, pages, read) {
 
 function addBookToLibrary(title, author, pages, read) {
     myLibrary.push(new Book(title, author, pages, read));
-}
 
-// MAIN
-addBookToLibrary('The Hobbit', 'J.R.R. Tolkien', 295, true);
-addBookToLibrary('Mind Games', 'Nora Roberts', 421, false);
-addBookToLibrary('Chi\'s Sweet Adventures Vol. 3', 'Konami Kanata', 88, true);
-addBookToLibrary('Erased Vol. 1', 'Kei Sanbe', 392, false);
-console.log(myLibrary);
-
-// display each book in the library
-
-for (let book of myLibrary) {
-    // create a card for each book
+    // create a card for the new book
     const bookCard = document.createElement('div');
     bookCard.classList.add('book');
 
     // add content to the card
-    const title = document.createElement('h1');
-    const author = document.createElement('h2');
+    const titleText = document.createElement('h1');
+    const authorText = document.createElement('h2');
 
-    const description = document.createElement('p');
+    const descriptionText = document.createElement('p');
 
-    title.textContent = book.title;
-    author.textContent = book.author;
-    description.textContent = `Pages: ${book.pages} | ${book.read ? 'Read' : 'Unread'}`;
+    titleText.textContent = title;
+    authorText.textContent = author;
+    descriptionText.textContent = `Pages: ${pages} | ${read ? 'Read' : 'Unread'}`;
 
     // update the DOM with the new card
-    bookCard.appendChild(title);
-    bookCard.appendChild(author);
-    bookCard.appendChild(description);
+    bookCard.appendChild(titleText);
+    bookCard.appendChild(authorText);
+    bookCard.appendChild(descriptionText);
     library.appendChild(bookCard);
 }
+
+// MAIN
